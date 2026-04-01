a51ec6695f7a4eb4c2628ecd5f1b769f8046e6ae
siradjevaga-ios <siradjevaga@gmail.com>
2026-04-01 19:27:43 +0400

    editing

--- NewspaperApp/Scene/Search/SearchManager.swift
+++ NewspaperApp/Scene/Search/SearchManager.swift
@@ -11,6 +11,6 @@ class SearchManager: SearchUseCase {
     func getSearch(query: String, completion: @escaping (NewsResponse?, String?) -> Void) {
         let urlString = "https://newsapi.org/v2/everything?q=\(query)&apiKey=0520b54b340441f28174ba6afe5742ef"
         completion(nil, nil)
-        NetworkManager.shared
+//        NetworkManager.shared
     }
 }
