060b8a940812ba8eeae95993c45bf0f6dfda6dab
siradjevaga-ios <siradjevaga@gmail.com>
2026-04-05 19:09:59 +0400

    bookmarkManager

--- NewspaperApp/Scene/Bookmark/BookmarViewModel.swift
+++ NewspaperApp/Scene/Bookmark/BookmarViewModel.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class BookmarViewModel {
+    private let useCase: BookmarkUseCase
+    var bookmarks = [Article]()
     
+    init(useCase: BookmarkUseCase) {
+        self.useCase = useCase
+    }
+    
+    func fethBookmarks(completion: @escaping(String?) -> Void) {
+        useCase.getBookmarks { [weak self]  downloadedArticles, error in
+            if let error {
+                completion(error.localizedDescription)
+                return
+            }
+            if let downloadedArticles {
+                self?.bookmarks = downloadedArticles
+                completion(nil)
+            }
+        }
+    }
+    
+    func toggleBookmark(article: Article, completion: @escaping (Error?) -> Void) {
+        let articleID = article.title ?? ""
+        
+    }
 }
